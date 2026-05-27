290ef3b5fa9cd2844f6037e810de97fd62ac963a
Anna-Paulina <134393584+Anna-Paulina@users.noreply.github.com>
2026-05-27 09:29:46 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/etf-tracker/', // change to your repo name
+  base: '/etf-tracker /', // change to your repo name
 })
